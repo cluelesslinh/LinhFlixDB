84e54d19455c2eec4f61a88d6129d7d6d5af4a4d
Linh Pham <77943437+cluelesslinh@users.noreply.github.com>
2023-10-16 18:44:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,18 +15,20 @@ const Directors = Models.Director;
 
 const app = express();
 
-// let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234'];
+const cors = require('cors')
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234',
+    'https://linhflixdb.cyclic.app/', 'https://linhflixdb.netlify.app/'];
 
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     if (!origin) return callback(null, true);
-//     if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-//       return callback(new Error(message), false);
-//     }
-//     return callback(null, true);
-//   }
-// }));
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+            let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+            return callback(new Error(message), false);
+        }
+        return callback(null, true);
+    }
+}));
 
 app.use(morgan("common"));
 app.use(bodyParser.json());
@@ -37,9 +39,6 @@ app.use((err, req, res, next) => {
     res.status(500).send("Something broke!");
 });
 
-const cors = require('cors');
-app.use(cors());
-
 require("./auth")(app);
 const passport = require("passport");
 require("./passport");
@@ -49,13 +48,8 @@ require("./passport");
 //     useUnifiedTopology: true
 // });
 
-mongoose.connect('mongodb+srv://cluelesslinh:kenshin@linhflixdb.ql71013.mongodb.net/?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-
-// mongoose.connect(process.env.CONNECTION_URI,
-//     { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true });
 
 // GET requests
 
